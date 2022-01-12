fe2196ee08f90533ff88ef8a44524badf88dfa4b
nzim-dev <deoverlordd@gmail.com>
2022-01-12 17:58:23 +0200

    added output formatter to string

--- item.go
+++ item.go
@@ -1,7 +1,19 @@
 package info
 
+import "fmt"
+
 //Item that we get after scraping
 type Item struct {
 	Title string
 	Link  string
+}
+
+func FormatOutput(items []Item) string {
+	var output string
+
+	for _, item := range items {
+		output += fmt.Sprintf("%s\n%s\n", item.Title, item.Link)
+	}
+
+	return output
 }
